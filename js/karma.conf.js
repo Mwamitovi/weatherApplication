ef9fb131d9dd235dc29fbfd35aff60bf2c76e2c5
Martin Matovu <matovu.synergy@gmail.com>
2019-03-07 16:33:23 +0300

    Added karma configuration

---
+++ js/karma.conf.js
@@ -0,0 +1,96 @@
+// Karma configuration
+// Generated on Thu Mar 07 2019 13:02:17 GMT+0300 (East Africa Time)
+
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine-jquery','jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        
+        { // Source files
+            pattern: 'js-src/*.js',
+            watched: true,
+            served: true,
+            included: true
+        },
+        
+        { // Spec files
+            pattern: 'js-test/**/*Spec.js',
+            watched: true,
+            served: true,
+            included: true
+        },
+        
+        { // fixtures
+            pattern: 'js-test/**/fixtures/*.html',
+            included: false,
+            served: true
+        }
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // Which plugins to enable
+    plugins: [
+      'karma-jasmine',
+      'karma-jasmine-jquery',
+    ],
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
