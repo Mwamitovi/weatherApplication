90ad3d6a895b77b08e80b3b3378a6d9284036333
Martin Matovu <matovu.synergy@gmail.com>
2019-05-06 20:23:40 +0300

    Writing specs for given requirements.

---
+++ js/js-test/spec/InventoryStockSpec.js
@@ -0,0 +1,54 @@
+
+(function () {
+    'use strict';
+    
+    /**
+    * If <MwamiTovi> is a company developing software for sales and inventory control systems.
+    * And currently, they have one base product that offers key features required of a sales & inventory system
+    * (e.g, generating sales invoice, sales return/issue, billing management, budgeting, , stock update, e.t.c). 
+    * They also customize base products as per customers' specific needs.
+    * Recently, the <MwamiTovi> company has provided software to a spare parts company,
+    * and the customer is performing acceptance testing for the inventory system.
+    * 
+    * The Store Administrator wants to update stock on every new transaction,
+    * so that they can get the balance/stock in hand for further usage.
+    * 
+    * Let us therefore consider some scenarios to update inventory stock in the event of any transaction.
+    * // Scenario-1: Inventory Stock should be updated on account of item(s) sale or issue of item(s)
+    * // Scenario-2: Inventory stock should be updated on return of any item(s)
+    * // Scenario-3: Inventory stocks should be updated on receiving/procuring new item(s)
+    */
+
+    // Writing specs for given requirements
+    describe('Inventory Stock should be updated', function() {
+        // Scenario-1
+        it('On sale of an item', function() {
+            var stockinhand_item1 = 11;
+            var item1 = 1;
+            var transaction = 'SALE';
+            expect(stockinhand_item1 - item1).toEqual(10);
+        });
+        it('On issue of an item within the organization', function() {
+            var stockinhand_item1 = 11;
+            var item1 = 1;
+            var transaction = 'ISSUE';
+            expect(stockinhand_item1 - item1).toEqual(10);
+        });
+
+        // Scenario-2
+        it('On return of any item', function() {
+            var stockinhand_item1 = 11;
+            var item1 = 1;
+            var transaction = 'SALE RETURN';
+            expect(stockinhand_item1 + item1).toEqual(12);
+        });
+
+        // Scenario-3
+        it('On receiving or procurring new item', function() {
+            var stockinhand_item1 = 11;
+            var item1 = 1;
+            var transaction = 'PROCUREMENT';
+            expect(stockinhand_item1 + item1).toEqual(12);
+        });
+    });
+})();
