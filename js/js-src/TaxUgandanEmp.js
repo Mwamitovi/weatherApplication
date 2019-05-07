99db4e656b190d3abd1d9c29effe3adfb5ec3144
Martin Matovu <matovu.synergy@gmail.com>
2019-05-07 18:34:41 +0300

    Updated the calculateTDS() to include other "if" clauses (tests pass)

--- js/js-src/TaxUgandanEmp.js
+++ js/js-src/TaxUgandanEmp.js
@@ -19,8 +19,11 @@ TaxUgandanEmp.prototype.calculateTDS = function() {
     if(this.getIncome() > 250000 && this.getIncome() <= 500000) {
         myTax = (this.getIncome() - 250000) * 10 / 100;
     }
-    if(this.getIncome() > 500000) {
-        myTax = (this.getIncome() - 200000) * 15 / 100;
+    else if(this.getIncome() > 500000 && this.getIncome() <= 1000000) {
+        myTax = (this.getIncome() - 500000) * 20 / 100;
+    }
+    else if(this.getIncome() > 1000000) {
+        myTax = (this.getIncome() - 1000000) * 30 / 100;
     }
     return myTax;
 };
