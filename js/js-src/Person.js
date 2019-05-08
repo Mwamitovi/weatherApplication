9d13d848d4f86ddbb1959b6ccd856583177feb7b
Martin Matovu <matovu.synergy@gmail.com>
2019-05-08 12:32:08 +0300

    Created a src-file, Person.js and added a Person() object

---
+++ js/js-src/Person.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+// defining a binding Person() object
+var Person = function(age, firstName, secondName) {
+    this.age = age;
+    this.firstName = firstName;
+    this.secondName = secondName;
+};
