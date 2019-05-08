d9fd4fcc3b27055b391113dea3bdd3ac6f957c79
Martin Matovu <matovu.synergy@gmail.com>
2019-05-08 11:42:54 +0300

    Added MyCustomEquality(), a custom tester. And declared it in beforeEach() (test passes)

--- js/js-test/spec/customEqualitySpec.js
+++ js/js-test/spec/customEqualitySpec.js
@@ -9,8 +9,11 @@
      * // Scenario-1: Two strings should be equal if the first word of both strings is "Kampala"
      */
    
-    // Understanding custom matchers
+    // Understanding custom equality tester
     describe('Custom Equality function: ', function() {
+        beforeEach(function() {
+            jasmine.addCustomEqualityTester(MyCustomEquality);
+        });
         // Scenario-1
         describe('Strings: ', function() {
             it('Should be custom equal, if first word of both the strings is "Kampala"', function() {
@@ -18,6 +21,20 @@
             });
         });
     });
+
+    /**
+     * Defining a custom equality tester function
+     * 
+     * In this example, MyCustomEquality() receives the first argument (the Actual Value)
+     * as the value passed to expect() and the value passed to the toEqual() matcher itself
+     * as the second argument or Expected value.
+     */
+    // 
+    var MyCustomEquality = function(strFirstString, strSecondString) {
+        if(typeof strFirstString == "string" && typeof strSecondString == "string") {
+            return strFirstString.substr(0,7) == strSecondString.substr(0,7);
+        };
+    };
 
 })();
 
