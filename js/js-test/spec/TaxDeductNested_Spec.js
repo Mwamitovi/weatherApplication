172168747e09def6bae70b607a0c067662cbe0f5
Martin Matovu <matovu.synergy@gmail.com>
2019-05-08 10:35:42 +0300

    Updated nested test-Suite describe('Tax deducted'), with block-level bindings

--- js/js-test/spec/TaxDeductNested_Spec.js
+++ js/js-test/spec/TaxDeductNested_Spec.js
@@ -64,13 +64,13 @@
     
     // Refactoring Jasmine tests for "existing code" using TDD/BDD
     describe('Employees of <MwamiTovi> Company: ', function() {
-        
-        describe('Tax Deduction at Source, ', function() {
+        // nested block, level-1
+        describe('Tax Deduction at Source (TDS) Currency: ', function() {
             var index = 0;
             var myRegion, myCurrency;
     
             beforeEach(function() {
-                myRegion = ["INDIA", "UK", "US"];
+                myRegion = ["UGANDA", "UK", "US"];
                 myCurrency = new Currency(myRegion[index]);
             });
     
@@ -86,65 +86,64 @@
             it('GBP (Pound, UK£) currency should be used', function() {
                 expect(myCurrency.currency).toBe("UK£");
             });
-            // Scenario-2
+            // Scenario-3
             it('USD (Dollar, US$) currency should be used', function() {
                 expect(myCurrency.currency).toBe("US$");
             });
         });
+        // nested block, level-1
+        describe('Tax deducted: ', function() {
+            // nested block, level-2
+            describe('For Ugandan Employees, ', function() {
+                var index=0;
+                var grossTaxableIncome, myTaxableIncome;
 
-        describe('Tax deducted for Ugandan Employees, ', function() {
-            // it('USH (Shilling, Ushs) currency should be used', function() {
-            //     expect(myCurrency.currency).toBe("Ushs");
-            // });
-            // Scenario-4
-            it('Should be 10%, if Gross Income is between USH.250,000/- and USH.500,000/-', function(){
-                var myTaxableIncome = new TaxUgandanEmp();
-                //Let's assume the taxable income is USH.300,000/-
-                myTaxableIncome.setIncome(300000);
-                expect(myTaxableIncome.calculateTDS()).toEqual(5000);
-            });
+                beforeEach(function() {
+                    grossTaxableIncome = [300000, 700000, 1300000];
+                    myTaxableIncome = new TaxUgandanEmp();
+                    myTaxableIncome.setIncome(grossTaxableIncome[index]);
+                });
 
-            // Scenario-5
-            it('Should be deducted 20%, ' +
-                'if Gross Income is between USH.500,000/- and USH.1,000,000/-', function(){
-                var myTaxableIncome = new TaxUgandanEmp();
-                //Let's assume the taxable income is USH.700,000/-
-                myTaxableIncome.setIncome(700000);
-                expect(myTaxableIncome.calculateTDS()).toEqual(40000);                
-            });
-            
-            // Scenario-6
-            it('Should be deducted 30%, if Gross Income is >USH.1,000,000/-', function(){
-                var myTaxableIncome = new TaxUgandanEmp();
-                //Let's assume the taxable income is USH.1,300,000/-
-                myTaxableIncome.setIncome(1300000);
-                expect(myTaxableIncome.calculateTDS()).toEqual(90000);                
-            });
-        });
+                afterEach(function() {
+                    index = index + 1;
+                });
 
-        // Scenario-2
-        describe('Tax deducted for United Kingdom Employess, ', function() {
-            // it('GBP (Pound, UK£) currency should be used', function() {
-            //     expect(myCurrency.currency).toBe("UK£");
-            // });
-            //Scenario-7(1)
-            xit('Should be deducted 20%, ' + 
-                'if Gross Annual Income is between £10,000/- and £32,000/-', function(){
-            });
-        });
+                // Scenario-4
+                it('Should be 10%, if Gross Income is between USH.250,000/- and USH.500,000/-', function(){
+                    //Let's assume the taxable income is USH.300,000/-
+                    expect(myTaxableIncome.calculateTDS()).toEqual(5000);
+                });
 
-        // Scenario-3
-        describe('Tax deducted for United States Employess, ', function() {
-            // it('USD (Dollar, US$) currency should be used', function() {
-            //     expect(myCurrency.currency).toBe("US$");
-            // });
-            //Scenario-8(2)
-            xit('Should be deducted 35%, ' + 
-                'of Gross Taxable Income, if employee status is SINGLE', function(){
+                // Scenario-5
+                it('Should be deducted 20%, ' +
+                    'if Gross Income is between USH.500,000/- and USH.1,000,000/-', function(){
+                    //Let's assume the taxable income is USH.700,000/-
+                    expect(myTaxableIncome.calculateTDS()).toEqual(40000);                
+                });
+                
+                // Scenario-6
+                it('Should be deducted 30%, if Gross Income is >USH.1,000,000/-', function(){
+                    //Let's assume the taxable income is USH.1,300,000/-
+                    expect(myTaxableIncome.calculateTDS()).toEqual(90000);                
+                });
             });
-            //Scenario-9(3)
-            xit('Should be deducted 30%, ' +
-                'of Gross Taxable Income, if employee status is MARRIED', function(){
+            // nested block, level-2
+            describe('Tax deducted for United Kingdom Employees, ', function() {
+                // Scenario-7
+                xit('Should be deducted 20%, ' + 
+                    'if Gross Annual Income is between £10,000/- and £32,000/-', function(){
+                });
+            });
+            // nested block, level-2
+            describe('Tax deducted for United States Employees, ', function() {
+                // Scenario-8
+                xit('Should be deducted 35%, ' + 
+                    'of Gross Taxable Income, if employee status is SINGLE', function() {
+                });
+                // Scenario-9
+                xit('Should be deducted 30%, ' +
+                    'of Gross Taxable Income, if employee status is MARRIED', function() {
+                });
             });
         });
     });
