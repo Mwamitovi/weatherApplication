80e4995a10a578db89076bba95740df3df796995
Martin Matovu <matovu.synergy@gmail.com>
2019-05-15 06:36:00 +0300

    Created "expectedSpec", and added test-suite "JavaScript Exceptions"

---
+++ js/js-test/spec/exceptionSpec.js
@@ -0,0 +1,25 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * As a JS-developer, if you want to handle all the errors and validate error messages
+     * so that the end-user should not get stuck during any process.
+     * 
+     * Let's consider some scenarios, that all the system and user-defined errors should
+     * be handled throughout the application.
+     * // Scenario-1: Error should be thrown on any unexpected behavior or malfunction.
+     * // Scenario-2: Error messages should be consistent throughout the application.
+     */
+   
+    // Understanding tests for exceptions
+    describe('JavaScript Exceptions: ', function() {
+        // Scenario-1
+        describe('Validate Errors: ', function() {
+            it('Error should be thrown on any unexpected behavior or malfunctioning', function() {
+            });
+        });
+    });
+
+})();
+
