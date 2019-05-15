ae8abd87f96b4986b2a4a7ab960e1aff107a7aee
Martin Matovu <matovu.synergy@gmail.com>
2019-05-15 06:27:19 +0300

    Removed custom-related specs, and the src/Person

--- js/js-src/Person.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-
-// defining a binding Person() object
-var Person = function(age, firstName, secondName) {
-    this.age = age;
-    this.firstName = firstName;
-    this.secondName = secondName;
-};
