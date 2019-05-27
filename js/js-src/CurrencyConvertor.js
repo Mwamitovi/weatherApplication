c057677df50d87fec6ee865234afc7267dc487a0
Martin Matovu <matovu.synergy@gmail.com>
2019-05-27 12:05:12 +0300

    Updated convertedCurrency() method, and defined scope directly. Removed convertCurrenyValue()

--- js/js-src/CurrencyConvertor.js
+++ js/js-src/CurrencyConvertor.js
@@ -1,6 +1,7 @@
 
 'use strict';
-var CurrencyConvertor = function(number, fromCurrency, toCurrency, currencyExchangeRate) {
+
+const CurrencyConvertor = function(number, fromCurrency, toCurrency, currencyExchangeRate) {
     this.number = number;
     this.fromCurrency = fromCurrency.toUpperCase();
     this.toCurrency = toCurrency.toUpperCase();
@@ -9,20 +10,28 @@ var CurrencyConvertor = function(number, fromCurrency, toCurrency, currencyExcha
     this.convertedCurrency = 0;
 
     // Start - configured predefined exchange Rates
-    var objExchangeRate = {
-        "USH": {"USD": 0.0002667, "JPY": 0.03}
+    const objExchangeRate = {
+        "USH": {"USD": 0.00025, "JPY": 0.03}
     }; 
 
     // End - configured predefined exchange Rates
-    for(var indexfromCurrency in objExhangeRate) {
-        var objPredefinedExchangeRate = objExchangeRate[indexfromCurrency];
+    for(let indexfromCurrency in objExchangeRate) {
+        let objPredefinedExchangeRate = objExchangeRate[indexfromCurrency];
 
         if(this.fromCurrency == indexfromCurrency) {
-            for(var indextoCurrency in objPredefinedExchangeRate) {
+            for(let indextoCurrency in objPredefinedExchangeRate) {
                 if(this.toCurrency == indextoCurrency) {
-                    this.predefinedExchangeRate = currencyExchangeRate ||
-                        objPredefinedExchangeRate[indextoCurrency];
-                    this.convertedCurrency = convertCurrencyValue();
+                    this.predefinedExchangeRate = currencyExchangeRate || objPredefinedExchangeRate[indextoCurrency];
+                    // this.convertedCurrency = convertCurrencyValue();
+                    this.convertedCurrency = function () {
+                        if(this.number > 10000) {
+                            // Exchange rate will be increased by 10%, if currency value is more than 10,000
+                            this.predefinedExchangeRate += (this.predefinedExchangeRate * 0.10);
+                            return Math.round(this.number * this.predefinedExchangeRate);
+                        } else {
+                            return this.number * this.predefinedExchangeRate;
+                        }
+                    };
                 }
             }
         }
@@ -30,12 +39,12 @@ var CurrencyConvertor = function(number, fromCurrency, toCurrency, currencyExcha
 };
 
 
-function convertCurrencyValue() {
-    if(this.number > 1000) {
-        // Exchange rate will be increased by 10%, if currency value is more than 10,000
-        this.predefinedExchangeRate += (this.predefinedExchangeRate * 0.10);
-        return Math.round(this.number * this.predefinedExchangeRate);
-    } else {
-        return this.number * this.predefinedExchangeRate;
-    };
-};
+// function convertCurrencyValue() {
+//     if(this.number > 10000) {
+//         // Exchange rate will be increased by 10%, if currency value is more than 10,000
+//         this.predefinedExchangeRate += (this.predefinedExchangeRate * 0.10);
+//         return Math.round(this.number * this.predefinedExchangeRate);
+//     } else {
+//         return this.number * this.predefinedExchangeRate;
+//     }
+// };
