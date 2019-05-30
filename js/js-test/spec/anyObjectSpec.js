c7c450e9c166075a8b5367bf4de9296c3cb4c475
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 12:54:44 +0300

    Created spec-file, "anyObjectSpec.js", and added pending test case for scenario-1

---
+++ js/js-test/spec/anyObjectSpec.js
@@ -0,0 +1,27 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * 
+     * jasmine.any(), jasmine.objectContaining()
+     * 
+     * "As a JavaScript developer, 
+     * I want to develop Jasmine tests using jasmine.any() and jasmine.objectContaining(),
+     * so that I can implement a test condition successfully."
+     * 
+     * Let's consider some scenarios in the current context, that is,
+     * jasmine.any() and jasmine.objectContaining() should be applied for different test conditions,
+     * // Scenario-1: The jasmine.any() should be applied successfully for comparing types of arguments.
+     * // Scenario-2: The jasmine.objectContaining() should match/compare objects corresponding to actual key/value pairs.
+     * // Scenario-3: The jasmine.objectContaining() should be applied successfully to compare arguments with Jasmine Spies.
+     */
+
+    describe("jasmine.any(), ", function() {
+        // scenario-1
+        it("should be applied successfully for comparing arguments", function(){
+        });
+    });
+
+})();
+
