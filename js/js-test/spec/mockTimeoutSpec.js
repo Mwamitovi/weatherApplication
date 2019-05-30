42dd2e0ecff411cb95898036b2d9cf122d60fc7b
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 16:42:50 +0300

    Added test code for scenario-1, to test setTimeout() and setInterval() using jasmine.clock()

--- js/js-test/spec/mockTimeoutSpec.js
+++ js/js-test/spec/mockTimeoutSpec.js
@@ -7,7 +7,7 @@
      * jasmine.clock()
      * 
      * "As a JavaScript developer, 
-     * I want to develop Jasmine tests for JavaScript timeout functions using Jasmine Clock
+     * I want to develop Jasmine tests for JavaScript timeout methods using Jasmine Clock
      * so that I can implement a test condition successfully."
      * 
      * Let's consider a scenario in the current context, that is, 
@@ -16,13 +16,47 @@
      */
     
      // scenario-1
-    describe("JavaScript Timeout Functions", function() {                       
+    describe("JavaScript Timeout Functions", function() { 
+        var mytimerCallback;
+        beforeEach(function(){
+            mytimerCallback = jasmine.createSpy("mytimerCallback");
+            jasmine.clock().install();
+        });
+
+        afterEach(function(){
+            jasmine.clock().uninstall();
+        });
+
         describe("With setTimeout() method:, ", function() {        
             it("should be called synchronously using Jasmine Clock", function(){
+                setTimeout(function(){
+                    mytimerCallback();
+                }, 100);
+
+                expect(mytimerCallback).not.toHaveBeenCalled();
+                jasmine.clock().tick(100);
+                expect(mytimerCallback).toHaveBeenCalled();
             });
         });
         describe("With setInterval() method:", function() {                
             it("should be called synchronously using Jasmine Clock", function(){
+                setInterval(function(){
+                    mytimerCallback();
+                }, 100);
+
+                expect(mytimerCallback).not.toHaveBeenCalled();
+
+                jasmine.clock().tick(101);
+                expect(mytimerCallback.calls.count()).toEqual(1);
+
+                jasmine.clock().tick(50);
+                expect(mytimerCallback.calls.count()).toEqual(1);
+
+                jasmine.clock().tick(50);
+                expect(mytimerCallback.calls.count()).toEqual(2);
+
+                jasmine.clock().tick(100);
+                expect(mytimerCallback.calls.count()).toEqual(3);
             });       
         });
     });
