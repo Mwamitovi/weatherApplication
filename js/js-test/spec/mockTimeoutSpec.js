069c5927296aa2668cdf347de419304647ed4e6c
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 16:18:00 +0300

    Created spec-file, "mockTimeoutSpec.js", and added pending test case for scenario-1

---
+++ js/js-test/spec/mockTimeoutSpec.js
@@ -0,0 +1,31 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * 
+     * jasmine.clock()
+     * 
+     * "As a JavaScript developer, 
+     * I want to develop Jasmine tests for JavaScript timeout functions using Jasmine Clock
+     * so that I can implement a test condition successfully."
+     * 
+     * Let's consider a scenario in the current context, that is, 
+     * Jasmine tests should be created for JavaScript timeout functions,
+     * // Scenario-1: JavaScript Timeout function should be called synchronously using Jasmine Clock
+     */
+    
+     // scenario-1
+    describe("JavaScript Timeout Functions", function() {                       
+        describe("With setTimeout() method:, ", function() {        
+            it("should be called synchronously using Jasmine Clock", function(){
+            });
+        });
+        describe("With setInterval() method:", function() {                
+            it("should be called synchronously using Jasmine Clock", function(){
+            });       
+        });
+    });
+
+})();
+
