51b83295c48e0da53bb86808363b0ca31dd8eeb1
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 11:50:52 +0300

    Created spec-file, customSpySpec.js, and added pending test cases for scenarios-1 & -2

---
+++ js/js-test/spec/customSpySpec.js
@@ -0,0 +1,29 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * 
+     * Custom Spy
+     * 
+     * To understand how to write Jasmine tests with your own custom method, 
+     * let's consider a few more scenarios as follows, 
+     * // Scenario-1: Person with O+ blood group can receive blood from a person with O+ blood group
+     * // Scenario 2: Person with O+ blood group can give blood to a person with A+ blood group
+     * // Scenario 3: Person with B- blood group can receive blood from a person with B- blood group
+     * // Scenario 4: Person with B- blood group can receive blood from a person with O- blood group
+     */
+
+    describe("<MwamiTovi> Company: Health Care Solution, ", function() {
+        describe("When to donate or receive blood, ", function() {
+            describe("Person with O+ Blood Group: ", function(){
+                it("can receive blood from a person with O+ blood group", function(){
+                });
+                it("can give blood to a person with A+ blood group", function(){
+                });
+            });
+        });
+    });
+
+})();
+
