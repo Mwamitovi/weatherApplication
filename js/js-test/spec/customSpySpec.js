201e4e21b0c30738a10a51f66f4bb662ae29cfb3
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 12:01:11 +0300

    Added test code for scenario-1, verify if/not callback is called. And verify if MatchBloodGroupToGiveOrReceive() is called, it hands back coontrol to ValidateBloodGroup()

--- js/js-test/spec/customSpySpec.js
+++ js/js-test/spec/customSpySpec.js
@@ -18,6 +18,22 @@
         describe("When to donate or receive blood, ", function() {
             describe("Person with O+ Blood Group: ", function(){
                 it("can receive blood from a person with O+ blood group", function(){
+                    var testPersonCriteria = new Person("Wasswa Ddumba", "10/30/1975", "O+", "Receiver");
+                    spyOn(testPersonCriteria, "MatchBloodGroupToGiveOrReceive").and.callThrough();
+
+                    var callback = jasmine.createSpy();
+                    testPersonCriteria.ValidateBloodGroup(callback);
+                    // verify that if or not callback method is called
+                    expect(callback).toHaveBeenCalled();
+                    expect(callback.calls.any()).toEqual(true);
+                    expect(callback.calls.count()).toEqual(1);
+
+                    // verify if MatchBloodGroupTOGiveOrReceive() is called, and
+                    // check if control goes back to the function
+                    expect(testPersonCriteria.MatchBloodGroupToGiveOrReceive).toHaveBeenCalled();
+                    expect(testPersonCriteria.MatchBloodGroupToGiveOrReceive.calls.any()).toEqual(true);
+                    expect(testPersonCriteria.MatchBloodGroupToGiveOrReceive.calls.count()).toEqual(1);
+                    expect(testPersonCriteria.ValidateBloodGroup(callback)).toContain("O+");
                 });
                 it("can give blood to a person with A+ blood group", function(){
                 });
