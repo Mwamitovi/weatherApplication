250e87063fc36c476c8148d74b574960a5c4eb7d
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 17:43:59 +0300

    Added test code for scenarios-1 and -2, and a spy for "myCallback"

--- js/js-test/spec/AjaxTestSpec.js
+++ js/js-test/spec/AjaxTestSpec.js
@@ -18,6 +18,9 @@
             var request, myText, myCallback;
             beforeEach(function(){
                 jasmine.Ajax.install();
+                var myCallback = jasmine.createSpy("success");
+                ajaxRequest(myCallback);
+                var request = jasmine.Ajax.request.mostRecent();
             });
             
             afterEach(function(){
@@ -25,10 +28,14 @@
             });
             // scenario-1
             it("Method 'GET' should be passed through XMLHttpRequest request", function(){
+                expect(request.method).toBe('GET');
+                expect(myCallback).not.toHaveBeenCalled();
             });
             // scenario-2         
             it("url should be passed as '/some/url/?q=Cookbook' through XMLHttpRequest request", function(){
-            });       
+                expect(request.url).toBe('/some/url/?q=Cookbook');
+                expect(myCallback).not.toHaveBeenCalled();
+            });    
         });
     });
 
