40e7fe661c371ebc0cf4dab516eac8a3cf6c042e
Martin Matovu <matovu.synergy@gmail.com>
2019-05-30 17:18:27 +0300

    Created a source-files, html + js files, to demo AJAX

---
+++ js/js-src/myScript.js
@@ -0,0 +1,14 @@
+
+function ajaxRequest(myCallback){
+      var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function(args){
+        myCallback(this.responseText);
+    };
+    xmlhttp.open("GET", "/some/url/?q=Cookbook", true);
+    xmlhttp.send();
+}
+
+function myCallback(myText){
+    var myTextToDisplay = myText;
+    $("#myID").html(myTextToDisplay);
+}
