59d3e7ee395866df7cf9c85425addc8985299d80
Martin Matovu <matovu.synergy@gmail.com>
2019-05-31 15:36:29 +0300

    Created spec-file "jQueryTestSpec.js", and added pending test case for scenario-1: "correct URL passed to $.ajax object"

---
+++ js/js-test/spec/jQueryTestSpec.js
@@ -0,0 +1,30 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * 
+     * testing jQuery
+     * 
+     * "As a JavaScript developer, 
+     * I want to test the jQuery code so that I complete the Acceptance Criteria successfully."
+     * 
+     * Let's consider the following scenarios in current context:
+     * // Scenario-1: The correct URL should be passed to the $.ajax object
+     * // Scenario-2: The myCallback method should be called on successful response
+     * // Scenario-3: The showErrorMessage method should be called for any malfunctioning
+     * // Scenario-4: The DOM should be updated based on the server response
+     */
+    
+    describe("jQuery", function() {
+        // scenario-1
+        describe("Ajax calls: ", function() {
+            describe("with $.ajax: ", function() {
+                it("correct URL should be passed to $.ajax object", function(){
+                });
+            });
+        });
+    });
+
+})();
+
