f7ad56a29bb4762e6b75a050ae14c90cae87f0ab
Martin Matovu <matovu.synergy@gmail.com>
2019-05-31 18:03:34 +0300

    Added beforeEach(), with argument "done", and added test code for scenario-1

--- js/js-test/spec/asyncDoneSpec.js
+++ js/js-test/spec/asyncDoneSpec.js
@@ -16,8 +16,24 @@
     describe("Jasmine specs for Asynchronous operations: ", function() {
         // scenario-1
         describe("With Done() function:", function(){
+            var myCallback, showErrorMessage;
+            beforeEach(function(done) {
+                myCallback = jasmine.createSpy();
+                $.ajax({
+                    url: "spec/fixtures/myData.json",
+                    dataType: "json",
+                    success: function(responseResult) {
+                        myCallback(responseResult);
+                        done();
+                    },
+                    error: showErrorMessage,
+                    timeout: 5000
+                });
+            });
+
             it("should support JavaScript asynchronous operations", function(){
-            })
+                expect(myCallback).toHaveBeenCalled();
+            });
         });
     });
 
