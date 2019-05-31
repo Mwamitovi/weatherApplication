78711f7bfc54fcf3940db2297bb0a3a16e6acc83
Martin Matovu <matovu.synergy@gmail.com>
2019-05-31 17:58:32 +0300

    Created spec-file "asyncDoneSpec()", and added pending test suite, check if Done() support async js operations

---
+++ js/js-test/spec/asyncDoneSpec.js
@@ -0,0 +1,25 @@
+
+(function () {
+    'use strict';
+
+    /**
+     * 
+     * Asynchronous operations with Done()
+     * 
+     * "As a JavaScript developer, I want to create Jasmine specs for asynchronous operations,
+     * so that I can implement a test condition successfully."
+     * 
+     * Let's consider the following scenario in the current context:
+     * // Scenario-1: Jasmine should support JavaScript asynchronous operations using the done() function
+     */
+    
+    describe("Jasmine specs for Asynchronous operations: ", function() {
+        // scenario-1
+        describe("With Done() function:", function(){
+            it("should support JavaScript asynchronous operations", function(){
+            })
+        });
+    });
+
+})();
+
